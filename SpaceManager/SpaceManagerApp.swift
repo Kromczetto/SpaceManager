fd211c8d0436c1328fbbc0b5bd8a8fe85b1d8889
Kromczetto <jakubkromolowski@gmail.com>
2024-04-03 15:39:58 +0200

    Initial Commit

---
+++ SpaceManager/SpaceManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceManagerApp.swift
+//  SpaceManager
+//
+//  Created by Kuba Kromomołowski on 03/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SpaceManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
