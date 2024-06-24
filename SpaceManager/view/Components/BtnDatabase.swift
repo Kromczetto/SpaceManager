f249af684507de888e2f371c444b15831c56ec06
Kromczetto <jakubkromolowski@gmail.com>
2024-06-24 17:55:27 +0200

    Fixing bug with not showing QRcode and doing some code cleaning

---
+++ SpaceManager/view/Components/BtnDatabase.swift
@@ -0,0 +1,18 @@
+//
+//  BtnDatabase.swift
+//  SpaceManager
+//
+//  Created by Kuba Kromomołowski on 20/06/2024.
+//
+
+import SwiftUI
+
+struct BtnDatabase: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BtnDatabase()
+}
