ed5f99eb510d29279aa86a61d67c28588a8dc1e2
Kromczetto <jakubkromolowski@gmail.com>
2024-07-16 16:18:53 +0200

    Prepering for camera handler

--- SpaceManager/model/Photo.swift
+++ SpaceManager/model/Photo.swift
@@ -1,5 +1,5 @@
 //
-//  CameraViewModel.swift
+//  Photo.swift
 //  SpaceManager
 //
 //  Created by Kuba Kromomołowski on 16/07/2024.
--- SpaceManager/view/Components/CameraView.swift
+++ SpaceManager/view/Components/CameraView.swift
@@ -2,27 +2,17 @@
 //  CameraView.swift
 //  SpaceManager
 //
-//  Created by Kuba Kromomołowski on 28/06/2024.
+//  Created by Kuba Kromomołowski on 16/07/2024.
 //
 
 import SwiftUI
-import CodeScanner
 
 struct CameraView: View {
-    @Binding var messageFromQR: String
-    @Binding var isCameraOpen: Bool
-    
     var body: some View {
-        CodeScannerView(
-            codeTypes: [.qr],
-            completion: {
-                result in
-                if case let .success(code) = result {
-                    self.messageFromQR = code.string
-                    self.isCameraOpen = false
-                }
-            }
-        )
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
+#Preview {
+    CameraView()
+}
